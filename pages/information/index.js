19b102ff98d4a259b751d8f30662aece0ba6ee85
ricardo <ricardo.br.pi@gmail.com>
2022-11-23 00:06:31 -0300

    feat: add page information

---
+++ pages/information/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Button from "../../src/components/Button/index";
+
+export default function information() {
+  return (
+    <section>
+      <Button href="/contact">Next page</Button>
+    </section>
+  );
+}
